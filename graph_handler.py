7cdfc0bf61f88a95639b25ec0fd5174b5d793c6d
Ghannoum99 <ghannoumjihad59@gmail.com>
2023-02-28 23:33:55 +0100

    Implementing PageRank Algorithm in main.py

--- graph_handler.py
+++ graph_handler.py
@@ -4,7 +4,8 @@
 import linecache
 import numpy as np
 from numpy.linalg import norm
-np.seterr(divide='ignore', invalid='ignore')
+
+
 class GraphHandler:
 
     def __init__(self, filename):
@@ -72,49 +73,9 @@ class GraphHandler:
             print("\n")
             for j in range(size):
                 G[i][j] = 1/size
-                print("G[" + str(i) + "][" + str(j) + "] : ", "%.4f" % G[i][j], end="\t")
+                print("G[" + str(i) + "][" + str(j) + "] : ", "%.5f" % G[i][j], end="\t")
         return G
 
-
-if __name__ == '__main__':
-    graph = GraphHandler("test.txt")
-    graph.generate_graph()
-    size = graph.get_nodes_nb()
-    adj_matrix = graph.get_adj_matrix()
-    P = graph.get_transition_matrix(adj_matrix)
-    G = graph.get_matrix_G()
-
-    size = graph.get_nodes_nb()
-    damping_factor = 0.85
-    jumping_rate = 1 - damping_factor
-    tolerance = 0.000001
-
-    x = [0 for i in range(size)]
-    x[0] = 1
-
-    old_x = []
-    new_x = []
-    counter = 0
-
-    new_x = x
-
-    while True :
-        old_x = new_x
-        # P * x
-        new_x = np.array(P).dot(old_x)
-        # P * alpha * x
-        new_x = new_x * damping_factor
-        # (P * alpha * X) + ((1 - alpha) * G * X)
-        new_x = new_x + ((jumping_rate) * np.array(G).dot(old_x))
-
-        counter += 1
-        diff = [new - old for old, new in zip(old_x, new_x)]
-        N = norm(diff)
-
-        if N < tolerance:
-            break
-
-    print("\ncounter : ", counter)
 
 
 
---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# Authors : Ulysse Feillet - Jihad GHANNOUM
+
+import numpy as np
+from numpy.linalg import norm
+from graph_handler import GraphHandler
+
+
+def pageRank(P, damping_factor, tolerance):
+    jumping_rate = 1 - damping_factor
+
+    G = graph.get_matrix_G()
+
+    size = graph.get_nodes_nb()
+
+    x = [0 for i in range(size)]
+    x[0] = 1
+
+    old_x = []
+    new_x = []
+    counter = 0
+
+    new_x = x
+
+    while True:
+        old_x = new_x
+        # P * x
+        new_x = np.array(P).dot(old_x)
+        # P * alpha * x
+        new_x = new_x * damping_factor
+        # (P * alpha * X) + ((1 - alpha) * G * X)
+        new_x = new_x + ((jumping_rate) * np.array(G).dot(old_x))
+        print("\n")
+        for i in range(size):
+            print("new_x[" + str(i) + "] : ", "%.9f" % new_x[i], end="\t")
+
+        counter += 1
+        diff = [new - old for old, new in zip(old_x, new_x)]
+        # Normalisation
+        N = norm(diff)
+
+        if N < tolerance:
+            break
+
+    print("\ncounter : ", counter)
+
+if __name__ == '__main__':
+    damping_factor = 0.85
+    tolerance = 0.000001
+    graph = GraphHandler("test.txt")
+    graph.generate_graph()
+    size = graph.get_nodes_nb()
+    adj_matrix = graph.get_adj_matrix()
+    P = graph.get_transition_matrix(adj_matrix)
+
+    pageRank(P, damping_factor, tolerance)
